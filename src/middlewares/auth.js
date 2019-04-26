eefb0d10cfd89f7276fe0e6b1bc2668624d1e0d6
Weliton Lima <welitonlima@consulplan.br>
2019-04-26 16:57:09 -0300

    Alteração 3

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
 module.exports = (req, res, next) => {
 
     const authHeader = req.headers.authorization;
@@ -5,7 +8,21 @@ module.exports = (req, res, next) => {
     if(!authHeader)
         return res.status(401).send({ error: 'No token' });
 
+    const parts = authHeader.split(' ');
 
+    const [ scheme, token ] = parts;
+
+    if(!/^Bearer$/i.test(scheme))
+        return res.status(401).send({ error: 'Token mal informado'});
+
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err)
+            return res.status(401).send({ error: 'Token inválido'});
+
+        req.userid = decoded.id;
+
+        return next();
         
+    });
 
 }
